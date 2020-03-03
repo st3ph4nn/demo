0846d70f5770f33b7c759f001c0539e4b9d28c8f
Stephan N <wapps.snguyen@gmail.com>
2020-03-03 15:21:00 +0100

    added schema for postgres

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into bankaccount (bankaccountnr, balance)
+values ('NL78INGB7209131833', 900.75),('NL03ABNA4909169245', 5.30);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bankaccount
+(
+ bankaccountnr varchar(18) NOT NULL,
+ balance numeric NOT NULL ,
+ PRIMARY KEY (bankaccountnr)
+);
+
+CREATE TABLE transactions
+(
+
+)
