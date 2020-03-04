5d8041288c92b15a65e6a6a49a803e27d5a02771
Stephan N <wapps.snguyen@gmail.com>
2020-03-05 00:17:18 +0100

    added a few javadoc

--- src/main/java/com/cgi/bmg/demo/controller/TransactionsController.java
+++ src/main/java/com/cgi/bmg/demo/controller/TransactionsController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+/**
+ * Endpoint for retrieval of balance and sending an transaction
+ */
 @CrossOrigin(origins = "http://localhost:8000")
 @RestController
 public class TransactionsController {
@@ -15,22 +18,34 @@ public class TransactionsController {
     @Resource
     ITransactionService transactionService;
 
+    /**
+     * Get balance for a certain bankaccount
+     * @param bankAccount
+     * @return BankAccountModel
+     */
     @GetMapping(path = "/getbalance")
     public ResponseEntity getBankAccountBalance(BankAccountModel bankAccount) {
         // TODO: create custom exceptions getBankAccountBalance
         try {
             return ResponseEntity.ok(transactionService.getBankAccount(bankAccount));
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("Something is going wrong ");
+            return ResponseEntity.status(500).body("Cannot get balance at the moment");
         }
     }
 
+    /**
+     * send a transacction from a bankaccount to another certain amount
+     * @param transaction
+     * @return if the transaction was succesful or not
+     */
     @PostMapping(path = "/transaction", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> sendTransaction(@RequestBody TransactionModel transaction) {
         // TODO: create custom exceptions sendTransaction
-
-
-        return ResponseEntity.ok(transactionService.sendTransaction(transaction));
+        try {
+            return ResponseEntity.ok(transactionService.sendTransaction(transaction));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Cannot send transaction");
+        }
     }
 
 }
--- src/main/java/com/cgi/bmg/demo/dao/BankAccountDao.java
+++ src/main/java/com/cgi/bmg/demo/dao/BankAccountDao.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Data Access ofject to get data from database
+ */
 @Repository
 public class BankAccountDao implements IBankAccountDao {
 
@@ -21,6 +24,11 @@ public class BankAccountDao implements IBankAccountDao {
         this.template = template;
     }
 
+    /**
+     * get bank account information for a certain bankaccoutnr
+     * @param bankAccountModel
+     * @return bankAccountModel
+     */
     @Override
     public BankAccountModel getBalance(BankAccountModel bankAccountModel) {
         final String SELECT_BY_BANKACCOUNTNR = "select * from bankaccount where bankaccountnr =:bankaccountnr";
@@ -30,6 +38,11 @@ public class BankAccountDao implements IBankAccountDao {
         return bankAccountModelList.get(0);
     }
 
+    /**
+     * update transactions tabel with a bankaccountnr that sends an amount X to another bankaccountnr.
+     * @param transactionModel
+     * @return String
+     */
     @Override
     public String sendTransaction(TransactionModel transactionModel) {
         final String INSERT_TRANSACTION = "insert into transactions (bankaccountnr, amount, tobankaccountnr) " +
